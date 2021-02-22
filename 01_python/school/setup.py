562abdedf8a964687c4a394813409091308febbe
sgm1021 <manimahn12@gmail.com>
2021-02-22 17:28:24 +0900

    jupyter files

---
+++ 01_python/school/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    nume="dss",
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.0.1",
+    author_email="manimahn12@gmail.com"
+    zip_safe=False
+)
